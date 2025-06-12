d9711f667f5f3a754c5edf0051927c2e5139bde4
강도현 <kdh0063@gachita.kr>
2025-06-12 23:27:16 +0900

    메인 페이지 작업2

--- src/components/home/PhotoStudioFinder.tsx
+++ src/components/home/PhotoStudioFinder.tsx
@@ -1,8 +1,15 @@
 import React from "react";
 import HomeStyles from "@/styles/home.module.css";
 import KakaoMap from "../map/kakaoMap";
+import PhotoStudioList from "./PhotoStudioList";
+import { useRouter } from "next/router";
 
 function PhotoStudioFinder() {
+  const router = useRouter();
+  const pushMap = () => {
+    router.push("/map");
+  };
+
   return (
     <article>
       <div className={HomeStyles.photostudio_text_box}>
@@ -12,14 +19,23 @@ function PhotoStudioFinder() {
         <p className={HomeStyles.home_sub_title}>
           원하는 지역을 검색하고 가까운 셀프사진관을 찾아보세요.
         </p>
+        <input
+          className={HomeStyles.search_input}
+          type="search"
+          placeholder="원하는 지역을 검색해보세요."
+        />
       </div>
       <div className={HomeStyles.photostudio_map_wrapper}>
         <div className={HomeStyles.photostudio_kakao_map}>
           <KakaoMap />
         </div>
-        <div></div>
+        <div className={HomeStyles.photostudio_photo_list}>
+          <PhotoStudioList />
+        </div>
       </div>
-      <p className={HomeStyles.photostudio_more_btn}>더 많은 사진관 보러가기</p>
+      <p onClick={pushMap} className={HomeStyles.photostudio_more_btn}>
+        더 많은 사진관 보러가기
+      </p>
     </article>
   );
 }
