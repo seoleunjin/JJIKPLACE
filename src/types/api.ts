d723f3bc91018fb34a15e441d806ade6f814f005
seoleunjin <anah9026@gmail.com>
2025-06-16 11:49:46 +0900

    로컬 변경사항 커밋

--- src/types/api.ts
+++ src/types/api.ts
@@ -4,18 +4,5 @@ interface MapBounds {
   neLat: number;
   neLng: number;
 }
-interface MarkerType {
-  id: number;
-  name: string;
-  lat: number;
-  lng: number;
-}
 
-interface ClusterType {
-  name: string;
-  count: number;
-  lat: number;
-  lng: number;
-}
-
-export type { MapBounds, MarkerType, ClusterType };
+export type { MapBounds };
