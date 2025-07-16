aedb86f19803476efc2b649a5ac7a4888b6a77b8
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2025-07-16 10:16:42 +0900

    배포

--- src/api/store.ts
+++ src/api/store.ts
@@ -5,7 +5,7 @@ const StoreDetailApi = (ps_id: number) => {
 };
 
 const ImageGalleryApi = (ps_id: number, page: number) => {
-  return instance.get(`/studios/${ps_id}/images/?page=${page}`);
+  return instance.get(`/studios/${ps_id}/images?page=${page}`);
 };
 
 export { StoreDetailApi, ImageGalleryApi };
