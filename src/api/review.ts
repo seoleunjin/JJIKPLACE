3c4b18077bf1b098dd87d3ef469e82b53d69c663
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2025-07-16 10:14:27 +0900

    배포

--- src/api/review.ts
+++ src/api/review.ts
@@ -1,7 +1,7 @@
 import { instance } from "./apiClient";
 
 const StudioReviewList = (ps_id: number, offset: number) => {
-  return instance.get(`/studios/${ps_id}/reviews/?offset=${offset}`);
+  return instance.get(`/studios/${ps_id}/reviews?offset=${offset}`);
 };
 
 const ReviewDetaile = (ps_id: number, review_id: number) => {
