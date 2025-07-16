365163e9b80173945c845a2c7dfdf49799ea29ec
seoleunjin <118324444+seoleunjin@users.noreply.github.com>
2025-07-16 18:32:49 +0900

    배포를 위한 수정

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,54 +1,54 @@
-import axios, { AxiosError } from "axios";
-
 // 배포전 사용
-const instanceBase = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_MAIN_SERVER,
-});
+// import axios, { AxiosError } from "axios";
 
-const instance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_MAIN_SERVER,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+// const instanceBase = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_MAIN_SERVER,
+// });
 
-const authInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_MAIN_SERVER,
-  headers: {
-    "Content-Type": "application/json",
-  },
-});
+// const instance = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_MAIN_SERVER,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
-authInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("accessToken");
-  if (token && config.headers) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// const authInstance = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_MAIN_SERVER,
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// });
 
-const userInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_MAIN_SERVER,
-});
+// authInstance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("accessToken");
+//   if (token && config.headers) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 
-userInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("accessToken");
-  if (token && config.headers) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+// const userInstance = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_MAIN_SERVER,
+// });
+
+// userInstance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("accessToken");
+//   if (token && config.headers) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
 
 // 401 또는 403 에러시 accessToken 토큰 삭제
-const handleAuthError = (error: AxiosError) => {
-  if (error.response?.status === 401 || error.response?.status === 403) {
-    localStorage.removeItem("accessToken");
-    // alert("세션 만료 또는 인증 오류");
-  }
-  return Promise.reject(error);
-};
-authInstance.interceptors.response.use((res) => res, handleAuthError);
-userInstance.interceptors.response.use((res) => res, handleAuthError);
+// const handleAuthError = (error: AxiosError) => {
+//   if (error.response?.status === 401 || error.response?.status === 403) {
+//     localStorage.removeItem("accessToken");
+//     // alert("세션 만료 또는 인증 오류");
+//   }
+//   return Promise.reject(error);
+// };
+// authInstance.interceptors.response.use((res) => res, handleAuthError);
+// userInstance.interceptors.response.use((res) => res, handleAuthError);
 
 // 기본 interceptors 사용 코드, 위는 authInstance,userInstance 두개 합치려고 축약형
 // authInstance.interceptors.response.use(
@@ -68,49 +68,59 @@ userInstance.interceptors.response.use((res) => res, handleAuthError);
 //   }
 // );
 
-export { instance, authInstance, instanceBase, userInstance };
+// export { instance, authInstance, instanceBase, userInstance };
 
 // 배포 후 사용
-// import axios from "axios";
+import axios, { AxiosError } from "axios";
 
-// const baseURL = "/api";
+const baseURL = "/api";
 
-// const instanceBase = axios.create({
-//   baseURL,
-// });
+const instanceBase = axios.create({
+  baseURL,
+});
 
-// const instance = axios.create({
-//   baseURL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
+const instance = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-// const authInstance = axios.create({
-//   baseURL,
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// });
+const authInstance = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
 
-// authInstance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("accessToken");
-//   if (token && config.headers) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
+authInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
-// const userInstance = axios.create({
-//   baseURL,
-// });
+const userInstance = axios.create({
+  baseURL,
+});
 
-// userInstance.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("accessToken");
-//   if (token && config.headers) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
+userInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("accessToken");
+  if (token && config.headers) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
-// export { instance, authInstance, instanceBase, userInstance };
+const handleAuthError = (error: AxiosError) => {
+  if (error.response?.status === 401 || error.response?.status === 403) {
+    localStorage.removeItem("accessToken");
+    // alert("세션 만료 또는 인증 오류");
+  }
+  return Promise.reject(error);
+};
+authInstance.interceptors.response.use((res) => res, handleAuthError);
+userInstance.interceptors.response.use((res) => res, handleAuthError);
+
+export { instance, authInstance, instanceBase, userInstance };
